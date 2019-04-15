79c375e3174c195b8f701b7a7d21f84783012e52
Hanna Arfine <ha264@cornell.edu>
2019-04-15 10:33:15 -0400

    draw from variables

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -16,8 +16,22 @@ def search():
 		output_message = "Your search: " + query
 		location = ['East Village', 'Chelsea']
 		features = ['sunset', 'walk']
+		results = [
+			{'name': '875 Third Avenue', 
+			'type':'POPS', 
+			'area':'East Village', 
+			'features':['Indoor', 'Plants', 'Walk'],
+			'img': "static/images/nyc.jpg"
+			},
 
-		return render_template('results.html', loc_len = len(location), location=location, feat_len = len(features), features=features)
+			{'name': '550 Madison Avenue', 
+			'type':'POPS', 
+			'area':'Chelsea', 
+			'features':['Indoor', 'Seating', 'Food'],
+			'img': "static/images/nyc.jpg"
+			}
+		]
+		return render_template('results.html', loc_len = len(location), location=location, feat_len = len(features), features=features, results_len = len(results), results=results)
 	# return render_template('results.html', data=data)
 	
 
