4b7e20c5d132304bfc79a92abf69055667b121e5
Hanna Arfine <ha264@cornell.edu>
2019-04-26 12:04:22 -0400

    home button

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,12 +1,10 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
-# from .prototype1 import main as get_results
-# from .prototype1 import reviews_to_array
 from .data_analysis import main as get_results_updated
 from .data_analysis import good_types
 
-project_name = "Happy Campers"
+project_name = "Urban Gems"
 net_id = "Katie Schretter: kvs46, Emily Gyles: emg226, Hanna Arfine: ha264, Kyra Wisniewski: klw227 "
 
 @irsystem.route('/', methods=['GET'])
@@ -26,7 +24,7 @@ def search():
 		return render_template('results.html', loc_len = len(location), location=location, feat_len = len(features), features=features, proto_results=proto_results, len_results=len(proto_results))
 	else:
 		return render_template('map.html', keywords=keywords, len_words=len(keywords))
-	# return render_template('results.html', data=data)
 	
-
+@irsystem.route('/')
+def index(): pass
 
